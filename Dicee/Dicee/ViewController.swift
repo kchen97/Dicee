bc7436e740f1fee4b18cc8fc339bd82d5bb06977
Korman Chen <kormanchen@kormans-mbp>
2017-10-20 00:49:29 -0700

    Constructed UI with assets and connected to View Controllerr

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Korman Chen on 10/20/17.
+//  Copyright © 2017 Korman Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: Properties
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+}
+
