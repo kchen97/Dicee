bef1867a39d50fa7902641526bffd84190dc8e7b
Korman Chen <kormanchen@kormans-mbp>
2017-10-20 18:15:34 -0700

    Completed Implementation

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
     //MARK: Properties
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -28,6 +31,11 @@ class ViewController: UIViewController {
     //MARK: Actions
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        randomDiceIndex1 = Int(arc4random_uniform(6)) + 1
+        randomDiceIndex2 = Int(arc4random_uniform(6)) + 1
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
     }
 }
 
